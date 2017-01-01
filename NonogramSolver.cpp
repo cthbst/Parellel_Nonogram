41d8770f4dc46974dd2f0298e14dead17595767a
Terry Wang <qwe50120@gmail.com>
2017-01-01 17:05:04 +0800

    mutex test

--- NonogramSolver.cpp
+++ NonogramSolver.cpp
@@ -17,14 +17,17 @@ typedef char State ;
 
 stack< NonogramSolver<25,25> > stk_nono;
 pthread_mutex_t mutex;
+pthread_mutex_t mu_check;
 
 void mu_init()
 {
 	pthread_mutex_init(&mutex,NULL);
+	pthread_mutex_init(&mu_check,NULL);
 }
 void mu_des()
 {
 	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&mu_check);
 }
 
 void push_stk(NonogramSolver<25,25> A)
@@ -53,85 +56,96 @@ void p_outans()
 
 void* unrecursive_solver(void*)
 {
+	int is_empty;
+
 	restart:
 
 	pthread_mutex_lock(&mutex);
-	int check=stk_nono.empty();
-	
-
-
-	while (!check || is_conti)
 	{
-		//printf("aa\n");
-		if(check)
-		{	
-			pthread_mutex_unlock(&mutex);
-			goto restart;
-		}
-		//pthread_mutex_unlock(&mutex);
+		is_empty=stk_nono.empty();
+	}
+	pthread_mutex_unlock(&mutex);
 
 
-		//pthread_mutex_lock(&mutex);
-		NonogramSolver<25,25> solverA=stk_nono.top();
-		stk_nono.pop();
-		pthread_mutex_unlock(&mutex);
-
-		if(solverA.ans_outed)
+	pthread_mutex_lock(&mu_check);
+	{
+		if(!is_conti)
 		{
-			goto restart;
+			pthread_mutex_unlock(&mu_check);
+			return NULL;
 		}
+	}
+	pthread_mutex_unlock(&mu_check);
 
-		solverA.LogicSolve();
 
-		if(!solverA.GridOK())
-		{
-			goto restart;
-		}
+	if(is_empty)
+	{	
+		goto restart;
+	}
+
+	pthread_mutex_lock(&mutex);
+	
+	NonogramSolver<25,25> solverA=stk_nono.top();
+	stk_nono.pop();
+	
+	pthread_mutex_unlock(&mutex);
+
+
+	if(solverA.ans_outed)
+	{
+		goto restart;
+	}
+
+	solverA.LogicSolve();
+
+	if(!solverA.GridOK())
+	{
+		goto restart;
+	}
 
 		// need tag for finish !!!!
-		if(solverA.finish())
+	if(solverA.finish())
+	{
+		pthread_mutex_lock(&mu_check);
 		{
 			is_conti=0;
-
-			pthread_mutex_lock(&mutex);
 			outans=solverA;
+		}
+		pthread_mutex_unlock(&mu_check);
+
+		pthread_mutex_lock(&mutex);
+		{
 			while(!stk_nono.empty())
 			{
 				stk_nono.pop();
 			}
-			
-
-			//solverA.output();
-			//cout << "finish" << endl;
-			//solverA.ans_outed=1;
-			pthread_mutex_unlock(&mutex);
-			return NULL;
 		}
+		pthread_mutex_unlock(&mutex);
 
-		for (int i=1 ;i<=25 ;i++ ){
-			for (int j=1 ;j<=25 ;j++ ){
-				if (solverA.Row[i][j]==unknown){
+		return NULL;
+	}
 
-					pthread_mutex_lock(&mutex);
+	for (int i=1 ;i<=25 ;i++ ){
+		for (int j=1 ;j<=25 ;j++ ){
+			if (solverA.Row[i][j]==unknown){
+
+
+				pthread_mutex_lock(&mutex);
+				{
 					solverA.Row[i][j]=full;
 					stk_nono.push(solverA);
 
 					solverA.Row[i][j]=empty;
 					stk_nono.push(solverA);
-					pthread_mutex_unlock(&mutex);
-
-					goto restart;
 				}
+				pthread_mutex_unlock(&mutex);
+
+				goto restart;
 			}
 		}
-
-		pthread_mutex_lock(&mutex);
-		check=stk_nono.empty();
-		pthread_mutex_unlock(&mutex);
-
-
 	}
-	
+
+	goto restart;
 
 }
 
